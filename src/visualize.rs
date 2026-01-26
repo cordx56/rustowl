7798e055df4d62a4e49672e38da71b243b73f501
CHISEN Kaoru <cordx56@cordx.cx>
2026-01-26 05:00:28 +0000

    chore: change blue to cyan

--- src/visualize.rs
+++ src/visualize.rs
@@ -15,7 +15,7 @@ use std::path::Path;
 mod colors {
     pub const RESET: &str = "\x1b[0m";
     pub const GREEN: &str = "\x1b[32m";
-    pub const BLUE: &str = "\x1b[34m";
+    pub const BLUE: &str = "\x1b[36m";
     pub const MAGENTA: &str = "\x1b[35m";
     pub const YELLOW: &str = "\x1b[33m";
     pub const RED: &str = "\x1b[31m";
