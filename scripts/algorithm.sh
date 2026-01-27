b2672dac8d13dc3d20dcc51cf9beb6afc1dc329a
CHISEN Kaoru <cordx56@cordx.cx>
2026-01-27 03:06:46 +0000

    chore: add echo for test fail

--- scripts/algorithm.sh
+++ scripts/algorithm.sh
@@ -8,8 +8,10 @@ do
     for j in {1..2}
     do
         echo "Check f${i} v${j}"
-        RES="$(rustowl show f${i} v${j} 2> /dev/null)"
-        if [ "$RES" != "$(cat f${i}_v${j}.txt)" ]; then
+        RESULT="$(rustowl show f${i} v${j} 2> /dev/null)"
+        ASSERT="$(cat f${i}_v${j}.txt)"
+        if [ "$RESULT" != "$ASSERT" ]; then
+            diff -u <(echo "$RESULT") <(echo "$ASSERT")
             exit 1
         fi
     done
