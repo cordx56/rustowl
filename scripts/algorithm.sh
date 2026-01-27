31e3cdd8570a80aba457a1366497c164974aa495
CHISEN Kaoru <cordx56@cordx.cx>
2026-01-27 00:52:14 +0000

    chore: update algo check script

--- scripts/algorithm.sh
+++ scripts/algorithm.sh
@@ -3,16 +3,12 @@
 cd "$(dirname $0)"
 cd ../algo-tests
 
-F1_V1="$(rustowl show f1 v1)"
-if [ "$F1_V1" != "$(cat f1_v1.txt)" ]; then
-    return 1
-fi
-
 for i in {1..3}
 do
     for j in {1..2}
     do
-        RES="$(rustowl show f${i} v${j})"
+        echo "Check f${i} v${j}"
+        RES="$(rustowl show f${i} v${j} 2> /dev/null)"
         if [ "$RES" != "$(cat f${i}_v${j}.txt)" ]; then
             exit 1
         fi
