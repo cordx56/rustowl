ffba229cd88965ccf88a866cdb0678e526885ba2
CHISEN Kaoru <cordx56@cordx.cx>
2026-04-20 20:30:07 +0900

    feat(wip): place initialized

--- src/bin/core/analyze/dataflow_analyzer.rs
+++ src/bin/core/analyze/dataflow_analyzer.rs
@@ -1,4 +1,5 @@
 use super::*;
+use rustowl::utils;
 
 pub fn get_maybe_lives<'tcx>(
     tcx: TyCtxt<'tcx>,
@@ -11,7 +12,25 @@ pub fn get_maybe_lives<'tcx>(
         .map(|(local_id, rich_locations)| {
             (
                 local_id,
-                rich_locations_to_ranges(basic_blocks, &rich_locations),
+                utils::eliminated_ranges(rich_locations_to_ranges(basic_blocks, &rich_locations)),
+            )
+        })
+        .collect()
+}
+
+pub fn get_maybe_initialized<'tcx>(
+    tcx: TyCtxt<'tcx>,
+    body: &Body<'tcx>,
+    basic_blocks: &[MirBasicBlock],
+) -> HashMap<LocalId, Vec<Range>> {
+    let move_data = MoveData::gather_moves(tcx, body);
+    MaybeInitializedPlaces::new(tcx, body, &move_data)
+        .get_maybe_initialized(tcx, body, &move_data)
+        .into_iter()
+        .map(|(local_id, rich_locations)| {
+            (
+                local_id,
+                utils::eliminated_ranges(rich_locations_to_ranges(basic_blocks, &rich_locations)),
             )
         })
         .collect()
--- src/bin/core/compiler/dataflow_analysis.rs
+++ src/bin/core/compiler/dataflow_analysis.rs
@@ -1,7 +1,137 @@
 use super::*;
 
-use rustc_mir_dataflow::{Analysis, ResultsVisitor, visit_reachable_results};
+use rustc_mir_dataflow::{
+    Analysis, MaybeReachable, ResultsVisitor, move_paths::MovePathIndex, visit_reachable_results,
+};
 use std::collections::{HashMap, HashSet};
+
+impl_as_rustc!(
+    MoveData<'tcx>,
+    rustc_mir_dataflow::move_paths::MoveData<'tcx>,
+);
+impl<'tcx> MoveData<'tcx> {
+    pub fn gather_moves(tcx: TyCtxt<'tcx>, body: &Body<'tcx>) -> Self {
+        Self(<Self as AsRustc>::Rustc::gather_moves(
+            body.as_rustc(),
+            tcx.into_rustc(),
+            |_| true,
+        ))
+    }
+    fn base_local(&self, mpi: MovePathIndex) -> LocalId {
+        AsRustc::from_rustc(self.as_rustc().base_local(mpi))
+    }
+}
+
+impl_as_rustc!(
+    MaybeInitializedPlaces<'a, 'tcx>,
+    rustc_mir_dataflow::impls::MaybeInitializedPlaces<'a, 'tcx>,
+);
+impl<'a, 'tcx> MaybeInitializedPlaces<'a, 'tcx> {
+    pub fn new(tcx: TyCtxt<'tcx>, body: &'a Body<'tcx>, move_data: &'a MoveData<'tcx>) -> Self {
+        Self(<Self as AsRustc>::Rustc::new(
+            tcx.into_rustc(),
+            body.as_rustc(),
+            move_data.as_rustc(),
+        ))
+    }
+    pub fn get_maybe_initialized(
+        self,
+        tcx: TyCtxt<'tcx>,
+        body: &Body<'tcx>,
+        move_data: &'a MoveData<'tcx>,
+    ) -> HashMap<LocalId, Vec<RichLocation>> {
+        let mut visitor = InitializedPlacesVisitor::new(move_data);
+        let results =
+            self.into_rustc()
+                .iterate_to_fixpoint(tcx.into_rustc(), body.as_rustc(), None);
+        visit_reachable_results(body.as_rustc(), &results, &mut visitor);
+        visitor.collect()
+    }
+}
+
+struct InitializedPlacesVisitor<'a, 'tcx> {
+    move_data: &'a MoveData<'tcx>,
+    loc_maybe_initialized: Vec<(RichLocation, LocalId)>,
+}
+impl<'a, 'tcx> InitializedPlacesVisitor<'a, 'tcx> {
+    fn new(move_data: &'a MoveData<'tcx>) -> Self {
+        Self {
+            move_data,
+            loc_maybe_initialized: Default::default(),
+        }
+    }
+    fn push(&mut self, rich_location: RichLocation, move_path_index: MovePathIndex) {
+        let local = self.move_data.base_local(move_path_index);
+        self.loc_maybe_initialized.push((rich_location, local));
+    }
+    fn collect(self) -> HashMap<LocalId, Vec<RichLocation>> {
+        let mut result = HashMap::new();
+        for (rich_location, local_id) in self.loc_maybe_initialized {
+            result
+                .entry(local_id)
+                .or_insert_with(Vec::new)
+                .push(rich_location);
+        }
+        result
+    }
+}
+
+impl<'a, 'tcx> ResultsVisitor<'tcx, <MaybeInitializedPlaces<'a, 'tcx> as AsRustc>::Rustc>
+    for InitializedPlacesVisitor<'a, 'tcx>
+{
+    fn visit_after_early_statement_effect(
+        &mut self,
+        _analysis: &<MaybeInitializedPlaces<'a, 'tcx> as AsRustc>::Rustc,
+        state: &<<MaybeInitializedPlaces<'a, 'tcx> as AsRustc>::Rustc as Analysis<'tcx>>::Domain,
+        _statement: &rustc_middle::mir::Statement<'tcx>,
+        location: rustc_middle::mir::Location,
+    ) {
+        if let MaybeReachable::Reachable(mpic) = state {
+            for mpi in mpic.iter() {
+                self.push(RichLocation::Start(AsRustc::from_rustc(location)), mpi);
+            }
+        }
+    }
+    fn visit_after_primary_statement_effect(
+        &mut self,
+        _analysis: &<MaybeInitializedPlaces<'a, 'tcx> as AsRustc>::Rustc,
+        state: &<<MaybeInitializedPlaces<'a, 'tcx> as AsRustc>::Rustc as Analysis<'tcx>>::Domain,
+        _statement: &rustc_middle::mir::Statement<'tcx>,
+        location: rustc_middle::mir::Location,
+    ) {
+        if let MaybeReachable::Reachable(mpic) = state {
+            for mpi in mpic.iter() {
+                self.push(RichLocation::Mid(AsRustc::from_rustc(location)), mpi);
+            }
+        }
+    }
+    fn visit_after_early_terminator_effect(
+        &mut self,
+        _analysis: &<MaybeInitializedPlaces<'a, 'tcx> as AsRustc>::Rustc,
+        state: &<<MaybeInitializedPlaces<'a, 'tcx> as AsRustc>::Rustc as Analysis<'tcx>>::Domain,
+        _statement: &rustc_middle::mir::Terminator<'tcx>,
+        location: rustc_middle::mir::Location,
+    ) {
+        if let MaybeReachable::Reachable(mpic) = state {
+            for mpi in mpic.iter() {
+                self.push(RichLocation::Start(AsRustc::from_rustc(location)), mpi);
+            }
+        }
+    }
+    fn visit_after_primary_terminator_effect(
+        &mut self,
+        _analysis: &<MaybeInitializedPlaces<'a, 'tcx> as AsRustc>::Rustc,
+        state: &<<MaybeInitializedPlaces<'a, 'tcx> as AsRustc>::Rustc as Analysis<'tcx>>::Domain,
+        _statement: &rustc_middle::mir::Terminator<'tcx>,
+        location: rustc_middle::mir::Location,
+    ) {
+        if let MaybeReachable::Reachable(mpic) = state {
+            for mpi in mpic.iter() {
+                self.push(RichLocation::Mid(AsRustc::from_rustc(location)), mpi);
+            }
+        }
+    }
+}
 
 impl_as_rustc!(MaybeLiveLocals, rustc_mir_dataflow::impls::MaybeLiveLocals);
 impl MaybeLiveLocals {
